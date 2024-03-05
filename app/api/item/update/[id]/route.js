8986e73ffe120b18947ebfb68c3cb8d93329ffd0
shuhei0720 <shuhei_0720@icloud.com>
2024-03-06 01:48:40 +0900

    trychatch文、await, reqBodyを追加

--- app/api/item/update/[id]/route.js
+++ app/api/item/update/[id]/route.js
@@ -1,7 +1,14 @@
 // app/api/item/update/[id]/route.js
 
 import { NextResponse } from "next/server"
+import connectDB from "../../../../utils/database"
 
-export async function PUT() {
-    return NextResponse.json({message: "アイテム編集成功"})
+export async function PUT(request) {
+    const reqBody = await request.json()
+    try{
+        await connectDB()
+        return NextResponse.json({message: "アイテム編集成功"})
+    }catch(err){
+        return NextResponse.json({message: "アイテム編集失敗"})
+    }
 }
