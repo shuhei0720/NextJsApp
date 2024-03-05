ea395b8df9566551cd1cb14d78728c2a1c119835
shuhei0720 <shuhei_0720@icloud.com>
2024-03-06 01:50:36 +0900

    ItemModelのupdateOne()メソッドを追加

--- app/api/item/update/[id]/route.js
+++ app/api/item/update/[id]/route.js
@@ -2,11 +2,13 @@
 
 import { NextResponse } from "next/server"
 import connectDB from "../../../../utils/database"
+import {ItemModel } from "../../../../utils/schemaModels"
 
 export async function PUT(request) {
     const reqBody = await request.json()
     try{
         await connectDB()
+        await ItemModel.updateOne()
         return NextResponse.json({message: "アイテム編集成功"})
     }catch(err){
         return NextResponse.json({message: "アイテム編集失敗"})
