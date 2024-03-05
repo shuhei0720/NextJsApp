318a7bc15e395e2a27bd277a597f5d08d5c19a17
shuhei0720 <shuhei_0720@icloud.com>
2024-03-05 23:55:13 +0900

    mongooseをインストール

--- app/api/item/create/route.js
+++ app/api/item/create/route.js
@@ -6,3 +6,5 @@ export async function POST(request) {
     console.log(await request.json())
     return NextResponse.json({message: "アイテム作成"})
 }
+
+mongodb+srv://shuhei0720:<password>@cluster0.oxx8nhe.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0
