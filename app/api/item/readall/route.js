1e41a2bcafb01bbf967d3e2759ec72c0c18c08c4
shuhei0720 <shuhei_0720@icloud.com>
2024-03-06 01:27:24 +0900

    ALLアイテム読み取り処理をreadall/route.jsに追加

--- app/api/item/readall/route.js
+++ app/api/item/readall/route.js
@@ -2,8 +2,14 @@
 
 import { NextResponse } from "next/server"
 import connectDB from "../../../utils/database"
+import { ItemModel } from "../../../utils/schemaModels"
 
 export async function GET() {
-    connectDB()
-    return NextResponse.json({message: "アイテム読み取り成功（オール）"})
+    try{
+        await connectDB()
+        const allItems = await ItemModel.find()
+        return NextResponse.json({message: "アイテム読み取り成功（オール）", allItems: allItems})
+    }catch(err){
+        return NextResponse.json({message: "アイテム読み取り失敗（オール）"})
+    }
 }
