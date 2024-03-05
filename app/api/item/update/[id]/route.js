6292648733a10310aca294016a60aeab35296de9
shuhei0720 <shuhei_0720@icloud.com>
2024-03-06 01:54:48 +0900

    指定IDのItemを編集する処理を追加

--- app/api/item/update/[id]/route.js
+++ app/api/item/update/[id]/route.js
@@ -4,11 +4,11 @@ import { NextResponse } from "next/server"
 import connectDB from "../../../../utils/database"
 import {ItemModel } from "../../../../utils/schemaModels"
 
-export async function PUT(request) {
+export async function PUT(request,context) {
     const reqBody = await request.json()
     try{
         await connectDB()
-        await ItemModel.updateOne()
+        await ItemModel.updateOne({_id: context.params.id}, reqBody)
         return NextResponse.json({message: "アイテム編集成功"})
     }catch(err){
         return NextResponse.json({message: "アイテム編集失敗"})
