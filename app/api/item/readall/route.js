ba6dde34cf540f3b868e58ad6877c62a790fd133
shuhei0720 <shuhei_0720@icloud.com>
2024-03-06 01:16:56 +0900

    readall/route.jsにdatabaseと接続するための記述を追加

--- app/api/item/readall/route.js
+++ app/api/item/readall/route.js
@@ -1,7 +1,9 @@
 // app/api/item/readall/route.js
 
 import { NextResponse } from "next/server"
+import connectDB from "../../../utils/database"
 
 export async function GET() {
+    connectDB()
     return NextResponse.json({message: "アイテム読み取り成功（オール）"})
 }
