a19e5207830addef0f1ed5452c47e95bb633cd7b
shuhei0720 <shuhei_0720@icloud.com>
2024-03-06 01:57:20 +0900

    delete/[id]route.jsにItemModelのdeleteOne()メソッドの記述を追加

--- app/api/item/delete/[id]/route.js
+++ app/api/item/delete/[id]/route.js
@@ -2,10 +2,12 @@
 
 import { NextResponse } from "next/server"
 import connectDB from "../../../../utils/database"
+import { ItemModel } from "../../../../utils/schemaModels"   // 追加
 
-export async function DELETE() {
+export async function DELETE(request, context) {
     try{
         await connectDB()
+        await ItemModel.deleteOne({_id: context.params.id})   // 追加
         return NextResponse.json({message: "アイテム削除成功"})
     }catch(err){
         return NextResponse.json({message: "アイテム削除失敗"})
